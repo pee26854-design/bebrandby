28f45911c09c34a7cc837fd35cd261694f0a4721
peerapat200 <151804450+peerapat200@users.noreply.github.com>
2026-03-20 08:35:39 +0700

    separate frontend

--- dist/src/App.jsx
+++ dist/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 
 // User pages
 import Home from "./user/Home.jsx";
